af73ae89859f9be1bc3284626666bedc20bcfcfa
Renato Felipe Coutinho De Andrade <renato2coutinho@gmail.com>
2025-12-07 23:31:48 +0000

    Set up a Firebase backend

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,26 +1,26 @@
 'use client';
 
-import { initializeApp, type FirebaseApp } from 'firebase/app';
-import { getAuth, type Auth } from 'firebase/auth';
-import { getFirestore, type Firestore } from 'firebase/firestore';
-import { useMemo, type ReactNode } from 'react';
-import { firebaseConfig } from './config';
-import { FirebaseProvider } from './provider';
+import React, { useMemo, type ReactNode } from 'react';
+import { FirebaseProvider } from '@/firebase/provider';
+import { initializeFirebase } from '@/firebase';
 
 interface FirebaseClientProviderProps {
   children: ReactNode;
 }
 
-export function FirebaseClientProvider({
-  children,
-}: FirebaseClientProviderProps) {
-  const firebaseApp = useMemo(() => initializeApp(firebaseConfig), []);
+export function FirebaseClientProvider({ children }: FirebaseClientProviderProps) {
+  const firebaseServices = useMemo(() => {
+    // Initialize Firebase on the client side, once per component mount.
+    return initializeFirebase();
+  }, []); // Empty dependency array ensures this runs only once on mount
 
-  const value = useMemo(() => {
-    const auth = getAuth(firebaseApp);
-    const firestore = getFirestore(firebaseApp);
-    return { app: firebaseApp, auth, firestore };
-  }, [firebaseApp]);
-
-  return <FirebaseProvider value={value}>{children}</FirebaseProvider>;
+  return (
+    <FirebaseProvider
+      firebaseApp={firebaseServices.firebaseApp}
+      auth={firebaseServices.auth}
+      firestore={firebaseServices.firestore}
+    >
+      {children}
+    </FirebaseProvider>
+  );
 }
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,8 @@
-import type { FirebaseOptions } from 'firebase/app';
-
-export const firebaseConfig: FirebaseOptions = {
-  projectId: 'projeto-fred-bkp-0338633-4bdf4',
-  appId: '1:444524525534:web:f7538a25a54d31b93a4f4f',
-  apiKey: 'AIzaSyCp8nJQZCLbUmVFeI4avkq6kHkESta4WX4',
-  authDomain: 'projeto-fred-bkp-0338633-4bdf4.firebaseapp.com',
-  measurementId: '',
-  messagingSenderId: '444524525534',
+export const firebaseConfig = {
+  "projectId": "projeto-fred-bkp-0338633-4bdf4",
+  "appId": "1:444524525534:web:f7538a25a54d31b93a4f4f",
+  "apiKey": "AIzaSyCp8nJQZCLbUmVFeI4avkq6kHkESta4WX4",
+  "authDomain": "projeto-fred-bkp-0338633-4bdf4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "444524525534"
 };
