0a677d0bb1f6baabf54b1ff00a28a01024264629
Renato Felipe Coutinho De Andrade <renato2coutinho@gmail.com>
2025-12-07 22:26:32 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,10 @@
 import type { FirebaseOptions } from 'firebase/app';
 
-export const firebaseConfig: FirebaseOptions = JSON.parse(
-  process.env.NEXT_PUBLIC_FIREBASE_CONFIG || '{}'
-);
+export const firebaseConfig: FirebaseOptions = {
+  projectId: 'projeto-fred-bkp-0338633-4bdf4',
+  appId: '1:444524525534:web:f7538a25a54d31b93a4f4f',
+  apiKey: 'AIzaSyCp8nJQZCLbUmVFeI4avkq6kHkESta4WX4',
+  authDomain: 'projeto-fred-bkp-0338633-4bdf4.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '444524525534',
+};
